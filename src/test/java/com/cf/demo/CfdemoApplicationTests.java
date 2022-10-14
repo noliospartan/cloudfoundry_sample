518990e74e8602686472d46855ae54efc1a7331e
sumitsamson <sumit.samson@broadcom.com>
2022-10-14 13:37:17 +0530

    init files

---
+++ src/test/java/com/cf/demo/CfdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cf.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
